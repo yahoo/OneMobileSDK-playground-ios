892096ed38eb003945b7c46d1c3cf97d16440940
Andrey Moskvin <andrey.moskvin@teamaol.com>
2017-04-25 16:31:54 +0300

    Carthage integration example

---
+++ Examples/Carthage Integration/Carthage Integration/ViewController.swift
@@ -0,0 +1,30 @@
+//  Copyright © 2017 One by AOL : Publishers. All rights reserved.
+
+import UIKit
+import OneMobileSDK
+import PlayerControls
+
+class ViewController: UIViewController {
+    @IBAction func playVideoTouched(_ sender: Any) {
+        var provider = OneSDK.Provider.default
+        provider.context.sdk.environment = "stage"
+        provider.getSDK()
+            .then { $0.getPlayer(videoID: "577d09391313230df40d1893") }
+            .dispatch(on: .main)
+            .onSuccess { player in
+                let playerViewController = PlayerViewController()
+                playerViewController.contentControlsViewController = DefaultControlsViewController()
+                playerViewController.player = player
+                self.navigationController?.pushViewController(playerViewController, animated: true)
+            }
+            .onError { error in
+                let alert = UIAlertController(title: "Error",
+                                              message: "\(error)",
+                    preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK",
+                                              style: .default,
+                                              handler: nil))
+                self.present(alert, animated: true, completion: nil)
+        }
+    }
+}
