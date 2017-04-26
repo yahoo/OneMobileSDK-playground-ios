454b0e7d8fb667d00345bf8eec292f81209494d3
Andrey Moskvin <andrey.moskvin@teamaol.com>
2017-04-26 15:32:33 +0300

    WIP EnvrmntRenderer Carthage Integration

---
+++ Examples/EnvrmntSDK Integration (Carthage)/EnvrmntSDK Integration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EnvrmntSDK Integration
+//
+//  Created by amoskvin on 4/26/17.
+//  Copyright © 2017 One by Aol : Publishers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
