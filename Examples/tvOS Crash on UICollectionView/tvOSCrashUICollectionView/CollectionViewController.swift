320bcfcc994c5b3eb8ddcfe324f7bc36898b402c
Alexey Demedetskiy <alexey.demedetskiy@sigma.software>
2017-06-19 16:22:38 +0300

    Add correct VC lifecycle into cell.

--- Examples/tvOS Crash on UICollectionView/tvOSCrashUICollectionView/CollectionViewController.swift
+++ Examples/tvOS Crash on UICollectionView/tvOSCrashUICollectionView/CollectionViewController.swift
@@ -8,13 +8,18 @@ private let reuseIdentifier = "Cell"
 class CollectionViewController: UICollectionViewController {
 
     @IBOutlet weak var reloadButton: UIButton!
+    var activeAVPlayerViewController: AVPlayerViewController?
     
     override var preferredFocusEnvironments: [UIFocusEnvironment] {
         return [self.reloadButton, self.collectionView!]
     }
     
-    
     @IBAction func firstItemReloadAction(_ sender: Any) {
+        self.activeAVPlayerViewController?.willMove(toParentViewController: nil)
+        self.activeAVPlayerViewController?.view.removeFromSuperview()
+        self.activeAVPlayerViewController?.removeFromParentViewController()
+        self.activeAVPlayerViewController = nil
+        
         self.collectionView?.reloadItems(at: [IndexPath(row:0, section:0)])
     }
 
@@ -23,13 +28,17 @@ class CollectionViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 1
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
         
         let playerViewController = AVPlayerViewController()
+        self.activeAVPlayerViewController = playerViewController
+        playerViewController.willMove(toParentViewController: self)
+        self.addChildViewController(playerViewController)
         playerViewController.view.frame = CGRect(x: 0, y: 0, width: cell.frame.size.width, height: cell.frame.size.height)
         cell.contentView.addSubview(playerViewController.view)
+        playerViewController.didMove(toParentViewController: self)
     
         return cell
     }
