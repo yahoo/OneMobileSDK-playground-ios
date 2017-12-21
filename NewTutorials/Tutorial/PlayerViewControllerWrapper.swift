27281f1580e2124de5384cacd6565a1fab358c41
Bogdan Bilonog <bogdan.bilonog@sigma.software>
2017-12-21 10:53:55 +0200

    Add examples for Tutorial 2 – Custom UX

--- NewTutorials/Tutorial/PlayerViewControllerWrapper.swift
+++ NewTutorials/Tutorial/PlayerViewControllerWrapper.swift
@@ -8,7 +8,13 @@ import PlayerControls
 class PlayerViewControllerWrapper: UIViewController {
     struct Props {
         var player: Future<Result<Player>>?
+        // Using default controls but it's possible to use custom by subclassing from ContentControlsViewController and set it to contentControlsViewController
         var controlsViewController: ContentControlsViewController = DefaultControlsViewController()
+        var controlsColor: UIColor?
+        var isHiddenSomeControls = false
+        var liveDotColor: UIColor?
+        var sidebarProps: SideBarView.Props = []
+        var isFilteredSubtitles = false
     }
     
     var props = Props() {
@@ -29,10 +35,43 @@ class PlayerViewControllerWrapper: UIViewController {
                         completion: nil)
             }
             
+            func render(player: Player) {
+                playerViewController?.customizeContentControlsProps = { [weak self] props in
+                    // Mudifing content props only if content video can be played
+                    guard var player = props.player else { return props }
+                    guard var controls = player.item.playable else { return props }
+                    
+                    // Changing color of live dot indicator
+                    controls.live.dotColor = self?.props.liveDotColor
+                    
+                    // Hiding 10s seek button and setting button
+                    if self?.props.isHiddenSomeControls == true {
+                        controls.seekbar?.seeker.seekTo = nil
+                        controls.settings = .hidden
+                    }
+                    
+                    // Filtering subtitles by name
+                    if self?.props.isFilteredSubtitles == true {
+                        guard case .`internal`(var group) = controls.legible else { return props }
+                        guard let options = group?.options else { return props }
+                        group?.options = options.filter { !$0.name.contains("English") }
+                        controls.legible = .`internal`(group)
+                    }
+                    
+                    var props = props
+                    player.item = .playable(controls)
+                    props = .player(player)
+                    
+                    return props
+                }
+                
+                playerViewController?.player = player
+            }
+            
             isLoading = true
             player
                 .dispatch(on: .main)
-                .onSuccess { [weak self] in self?.playerViewController?.player = $0 }
+                .onSuccess(call: render)
                 .onError(call: show)
                 .onComplete { [weak self] _ in self?.isLoading = false }
         }
@@ -55,5 +94,13 @@ class PlayerViewControllerWrapper: UIViewController {
             activityIndicatorView.stopAnimating()
         
         playerViewController?.contentControlsViewController = props.controlsViewController
+        
+        // Changing color of content view controller controls
+        playerViewController?.view.tintColor = props.controlsColor
+        
+        // Adding sidebar buttons
+        if let defaultControlsViewController = playerViewController?.contentControlsViewController as? DefaultControlsViewController {
+            defaultControlsViewController.sidebarProps = props.sidebarProps
+        }
     }
 }
