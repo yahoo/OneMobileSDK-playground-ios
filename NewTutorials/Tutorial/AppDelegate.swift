172bce92481cc9f3adced0dc3f092f0da76560f6
Andrey Moskvin <andrey.moskvin@oath.com>
2018-08-22 14:55:00 +0300

    1. Hardware ringer behavior notes 2. Couple markdown links fixes 3. Swift and Xcode notes updates

--- NewTutorials/Tutorial/AppDelegate.swift
+++ NewTutorials/Tutorial/AppDelegate.swift
@@ -1,17 +1,28 @@
 //  Copyright © 2017 Oath. All rights reserved.
 
 import UIKit
-
+import AVKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        /*
+         This code ignores iOS hardware ringer silent mode - so
+         video will be playing with sound.
+         Use it if you want to override default behavior.
+        */
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+        } catch {
+            assertionFailure("Audio session `setCategory` failed!")
+        }
+
         return true
     }
-
 }
 
